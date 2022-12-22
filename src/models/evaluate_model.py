acb8449d71c06ed5118836873514cf02efca312a
bobcastaldeli <castaldelliroberto@gmail.com>
2022-12-21 23:56:21 -0300

    feat: add predictions outputs

--- src/models/evaluate_model.py
+++ src/models/evaluate_model.py
@@ -20,18 +20,19 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 
-if len(sys.argv) != 3 and len(sys.argv) != 5:
+if len(sys.argv) != 4 and len(sys.argv) != 5:
     sys.stderr.write("Arguments error. Usage:\n")
     sys.stderr.write(
-        "\tpython evaluate_model.py input_data-path train-model-path\n"
+        "\tpython evaluate_model.py input_data-path train-model-path"
+        " predictions-output-path\n"
     )
     sys.exit(1)
 
 
-input_path, model_path = sys.argv[1], sys.argv[2]
+input_path, model_path, prediction_path = sys.argv[1], sys.argv[2], sys.argv[3]
 test_input_path = os.path.join(input_path, "test.csv")
 model_input_path = os.path.join(model_path, "model.pkl")
-# predict_path = os.path.join(prediction_path, "predict.csv")
+predict_path = os.path.join(prediction_path, "predict.csv")
 # classf_report_path = os.path.join(report_path, "classification_report.png")
 # rocauc_path = os.path.join(report_path, "roc_auc_curve.png")
 # pr_path = os.path.join(report_path, "precision_recall_curve.png")
@@ -43,7 +44,7 @@ with open("params.yaml", "r", encoding="utf-8") as file:
     target = params["evaluate_model"]["target"]
 
 
-def predict_model(test_path, model):
+def predict_model(test_path, model, pred_path):
     """Predict the output of the model.
 
     params:
@@ -58,12 +59,12 @@ def predict_model(test_path, model):
     with open(model, "rb") as file:
         model = pickle.load(file)
     logger.info("Predicting output...")
-    # y_pred = model.predict(X_test)#
+    y_pred = model.predict(X_test)  #
     logger.info("Saving output...")
-    # os.makedirs(sys.argv[2], exist_ok=True)
-    # output = pd.DataFrame({"y_test": y_test, "y_pred": y_pred})
-    # output.to_csv(pred_path, index=False)
+    os.makedirs(sys.argv[3], exist_ok=True)
+    output = pd.DataFrame({"y_test": y_test, "y_pred": y_pred})
+    output.to_csv(pred_path, index=False)
 
 
 if __name__ == "__main__":
-    predict_model(test_input_path, model_input_path)
+    predict_model(test_input_path, model_input_path, predict_path)
