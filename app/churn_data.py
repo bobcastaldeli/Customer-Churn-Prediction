1c07a38bcc05b32c48811cd00f2e7952b76a351f
bobcastaldeli <castaldelliroberto@gmail.com>
2023-01-16 22:42:44 -0300

    feat: add data class of api

---
+++ app/churn_data.py
@@ -0,0 +1,28 @@
+"""This module defines the data that is expected in the request body."""
+
+from pydantic import BaseModel
+
+
+class ChurnData(BaseModel):
+    """This class defines the data that is expected in the request body when
+    calling the /predict endpoint."""
+
+    gender: str
+    SeniorCitizen: int
+    Partner: str
+    Dependents: str
+    tenure: int
+    PhoneService: str
+    MultipleLines: str
+    InternetService: str
+    OnlineSecurity: str
+    OnlineBackup: str
+    DeviceProtection: str
+    TechSupport: str
+    StreamingTV: str
+    StreamingMovies: str
+    Contract: str
+    PaperlessBilling: str
+    PaymentMethod: str
+    MonthlyCharges: float
+    TotalCharges: float
