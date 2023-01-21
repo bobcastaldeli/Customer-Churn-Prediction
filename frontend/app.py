2a84d51e4c9973b08dd846e182aaf19385f09625
bobcastaldeli <castaldelliroberto@gmail.com>
2023-01-20 23:43:33 -0300

    feat: change widgets order in frontend

--- frontend/app.py
+++ frontend/app.py
@@ -9,22 +9,20 @@ import streamlit as st
 
 
 st.title("Churn Prediction App")
-
 st.write("This app predicts whether a customer will churn or not.")
 
 
-col1, col2, col3, col4 = st.columns(4)
+gender = st.selectbox("Select your sex", ["Female", "Male"])
+
+col1, col2, col3 = st.columns(3)
 
 with col1:
-    gender = st.radio("Select your gender", ["Female", "Male"])
-
-with col2:
     senior_citizen = st.radio("Are you a senior citizen?", ["Yes", "No"])
 
-with col3:
+with col2:
     partner = st.radio("Do you have a partner?", ["Yes", "No"])
 
-with col4:
+with col3:
     dependents = st.radio("Do you have dependents?", ["Yes", "No"])
 
 tenure = st.slider("How many months have you been a customer?", 0, 72, 12)
@@ -37,50 +35,50 @@ with col1:
 with col2:
     multiple_lines = st.radio("Do you have multiple lines?", ["Yes", "No"])
 
+internet_service = st.selectbox(
+    "Which internet service do you have?", ["DSL", "Fiber optic", "No"]
+)
+
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    internet_service = st.radio(
-        "Which internet service do you have?", ["DSL", "Fiber optic", "No"]
-    )
-
-with col2:
     online_security = st.radio("Do you have online security?", ["Yes", "No"])
 
-with col3:
+with col2:
     online_backup = st.radio("Do you have online backup?", ["Yes", "No"])
 
-with col4:
+with col3:
     device_protection = st.radio(
         "Do you have device protection?", ["Yes", "No"]
     )
 
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
+with col4:
     tech_support = st.radio("Do you have tech support?", ["Yes", "No"])
 
-with col2:
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
     streaming_tv = st.radio("Do you have streaming TV?", ["Yes", "No"])
 
-with col3:
+with col2:
     streaming_movies = st.radio("Do you have streaming movies?", ["Yes", "No"])
 
-with col4:
-    contract = st.radio(
-        "Which contract do you have?",
-        ["Month-to-month", "One year", "Two year"],
+with col3:
+    paperless_billing = st.radio(
+        "Do you have paperless billing?", ["Yes", "No"]
     )
 
 col1, col2 = st.columns(2)
 
 with col1:
-    paperless_billing = st.radio(
-        "Do you have paperless billing?", ["Yes", "No"]
+    contract = st.selectbox(
+        "Which contract do you have?",
+        ["Month-to-month", "One year", "Two year"],
     )
 
 with col2:
-    payment_method = st.radio(
+    payment_method = st.selectbox(
         "Which payment method do you use?",
         [
             "Bank transfer (automatic)",
